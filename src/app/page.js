42af5e582defc120de237cdee4e45938ab324c7c
Andy <hinchliffe106@gmail.com>
2024-06-12 11:35:05 +0100

    gtag movement

--- src/app/page.js
+++ src/app/page.js
@@ -69,9 +69,8 @@ const [isLoaded, setIsLoaded] = useState(false);
     
 
   return (<>
-  <GoogleTagManager gtmId="G-K7SSN3HFSE" />
   <div className="sm:bg-[#9a9488] bg-[#d3caab]">
-  
+  <GoogleTagManager gtmId="G-K7SSN3HFSE" />
     <div className="bg-[url('/retro_radio.jpg')] sm:bg-contain  bg-center bg-no-repeat h-screen bg grid grid-cols-2">
     <div>
     {/* <div className="text-black text-xl font-bold absolute top-5 left-5 "><Link href="/info"><IoMdInformationCircleOutline /></Link></div> */}
